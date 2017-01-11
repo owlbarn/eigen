672287de65f2bb45b217fd0b12ed905c9e907a7b
Liang Wang <ryanrhymes@gmail.com>
2017-01-11 15:01:47 +0000

    add initial files

---
+++ lib/libowl_eigen.cpp
@@ -0,0 +1,16 @@
+/*
+ * OWL - an OCaml numerical library for scientific computing
+ * Copyright (c) 2016 Liang Wang <liang.wang@cl.cam.ac.uk>
+ */
+
+#include <iostream>
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+#include "libowl_eigen.h"
+
+using namespace Eigen;
+
+#include "libowl_eigen_s.cpp"
+#include "libowl_eigen_d.cpp"
+#include "libowl_eigen_c.cpp"
+#include "libowl_eigen_z.cpp"
