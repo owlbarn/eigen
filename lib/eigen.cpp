269fc741d283843dc82dfea4333cf9fa5b13c54c
Liang Wang <ryanrhymes@gmail.com>
2017-01-11 15:08:18 +0000

    change file names

---
+++ lib/eigen.cpp
@@ -0,0 +1,16 @@
+/*
+ * OWL - an OCaml numerical library for scientific computing
+ * Copyright (c) 2016 Liang Wang <liang.wang@cl.cam.ac.uk>
+ */
+
+#include <iostream>
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+#include "eigen.h"
+
+using namespace Eigen;
+
+#include "eigen_spmat_s.cpp"
+#include "eigen_spmat_d.cpp"
+#include "eigen_spmat_c.cpp"
+#include "eigen_spmat_z.cpp"
