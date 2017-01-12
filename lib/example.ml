ac2bc4166d178e83cba03c42d6912d6c3b2e19da
Liang Wang <ryanrhymes@gmail.com>
2017-01-12 17:49:37 +0000

    coding

--- lib/example.ml
+++
@@ -1,7 +0,0 @@
-
-let _ =
-  let x = Eigen.Dense.S.create 5 5 in
-  let _ = Ml_dsmat_s.set x 0 0 1. in
-  let _ = Ml_dsmat_s.set x 2 3 2. in
-  let _ = Ml_dsmat_s.set x 3 2 3. in
-  Ml_dsmat_s.print x
