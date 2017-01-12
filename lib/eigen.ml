84fded919782b76fcf2e7e0610cb7abffaacd8b7
Liang Wang <ryanrhymes@gmail.com>
2017-01-12 00:06:59 +0000

    coding

---
+++ lib/eigen.ml
@@ -0,0 +1,17 @@
+
+module Dense = struct
+
+  module S = Ml_dsmat_s
+
+  module D = Ml_dsmat_d
+
+  module C = Ml_dsmat_c
+
+  module Z = Ml_dsmat_z
+
+end
+
+
+module Sparse = struct
+  
+end
--- lib/example.ml
+++ lib/example.ml
@@ -1,6 +1,6 @@
 
 let _ =
-  let x = Ml_dsmat_s.create 5 5 in
+  let x = Eigen.Dense.S.create 5 5 in
   let _ = Ml_dsmat_s.set x 0 0 1. in
   let _ = Ml_dsmat_s.set x 2 3 2. in
   let _ = Ml_dsmat_s.set x 3 2 3. in
