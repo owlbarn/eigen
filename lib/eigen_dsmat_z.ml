e4869fc75fffd1fba76a665c473ddf8b2810b68a
Liang Wang <ryanrhymes@gmail.com>
2017-01-12 17:32:41 +0000

    coding

--- lib/eigen_dsmat_z.ml
+++ lib/eigen_dsmat_z.ml
@@ -15,6 +15,25 @@ let create m n =
 
 let delete x = ml_eigen_delete x
 
+let zeros m n =
+  let x = ml_eigen_zeros (Int64.of_int m) (Int64.of_int n) in
+  Gc.finalise ml_eigen_delete x;
+  x
+
+let ones m n =
+  let x = ml_eigen_ones (Int64.of_int m) (Int64.of_int n) in
+  Gc.finalise ml_eigen_delete x;
+  x
+
+let eye m =
+  let x = ml_eigen_eye (Int64.of_int m) in
+  Gc.finalise ml_eigen_delete x;
+  x
+
+let rows x = ml_eigen_rows x |> Int64.to_int
+
+let cols x = ml_eigen_cols x |> Int64.to_int
+
 let get x i j = ml_eigen_get x (Int64.of_int i) (Int64.of_int j)
 
 let set x i j a = ml_eigen_set x (Int64.of_int i) (Int64.of_int j) a
