9854a42506e93fd97392e43e3d99cff77226a596
Liang Wang <ryanrhymes@gmail.com>
2017-01-12 12:50:34 +0000

    update eigen.ml

--- lib/eigen.ml
+++ lib/eigen.ml
@@ -1,17 +1,17 @@
 
 module Dense = struct
 
-  module S = Ml_dsmat_s
+  module S = Eigen_dsmat_s
 
-  module D = Ml_dsmat_d
+  module D = Eigen_dsmat_d
 
-  module C = Ml_dsmat_c
+  module C = Eigen_dsmat_c
 
-  module Z = Ml_dsmat_z
+  module Z = Eigen_dsmat_z
 
 end
 
 
 module Sparse = struct
-  
+
 end
