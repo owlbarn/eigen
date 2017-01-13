6a82549e4567ed87bd3f13af4cb65f970a445576
Liang Wang <ryanrhymes@gmail.com>
2017-01-13 16:52:06 +0000

    update copyright info

--- lib/eigen_types.ml
+++ lib/eigen_types.ml
@@ -1,4 +1,4 @@
 (*
  * Eigen - an OCaml interface to C++ Eigen library
- * Copyright (c) 2016 Liang Wang <liang.wang@cl.cam.ac.uk>
+ * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
  *)
